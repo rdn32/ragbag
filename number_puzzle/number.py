fce9656743a01685954566f76dda3e72f2a9f6f5
Robin Newton <rdn32@my.open.ac.uk>
2012-01-15 21:18:48 +0000

    Implement lengthUnder

--- number_puzzle/number.py
+++ number_puzzle/number.py
@@ -2,17 +2,21 @@ base = None
 columns = None
 digits = None
 max_power = None
+lengths_under = None
 
 def init(base_, columns_, digits_):
-    global base, columns, digits, max_power
+    global base, columns, digits
+    global max_power, lengths_under
     assert base_ > 1
     assert len(digits_) == base_
     assert len(columns_) > 0
     assert columns_[0] == ""
+    assert digits_[0] == ""
     base = base_
     columns = columns_
     digits = digits_
     max_power = len(columns_)
+    lengths_under = {}
 
 def repr(n):
     remaining = n
@@ -20,9 +24,30 @@ def repr(n):
     for p in range(max_power):
         d = remaining % base
         remaining = remaining // base
-        if d == 0:
-            result.append("")
-        else:
-            result.append("%s%s" % (digits[d], columns[p]))
+        result.append(columnEntry(d, p))
     result.reverse()
     return "".join(result)
+
+def columnEntry(d, p):
+        if d == 0:
+            return ""
+        else:
+            return "%s%s" % (digits[d], columns[p])
+
+def lengthUnder(power, prefix):
+    if power == 0:
+        return (prefix, 1)
+
+    if lengths_under.has_key(power):
+        length, count = lengths_under[power]
+    else:
+        length = 0
+        count = 0
+        for d in range(base):
+            entry = columnEntry(d, power-1)
+            subLength, subCount = lengthUnder(power-1, len(entry))
+            count = count + subCount
+            length = length + subLength
+        lengths_under[power] = length, count
+        
+    return (length + prefix*count, count)
--- number_puzzle/test.py
+++ number_puzzle/test.py
@@ -7,14 +7,29 @@ class BaseTenTestCase(unittest.TestCase):
     def setUp(self):
         number.init(10,
                     ["", "ten", "hundred", "thousand"],
-                    ["zero", "one", "two", "three", "four",
+                    ["", "one", "two", "three", "four",
                      "five", "six", "seven", "eight", "nine"])
 
     def testRepr(self):
+        self.assertEquals("", number.repr(0))
         self.assertEquals("eight", number.repr(8))
         self.assertEquals("threetennine", number.repr(39))
         self.assertEquals("fourhundredonetenone", number.repr(411))
         self.assertEquals("fivethousandtwohundredseven", number.repr(5207))
+
+    def _testLengthsImpl(self, first, power):
+        num = 10 ** power
+        combined = 0
+        for n in range(first, first + num):
+            combined = combined + len(number.repr(n))
+        prefix = len(number.repr(first))
+        self.assertEquals((combined, num), number.lengthUnder(power, prefix))
+
+    def testLengths(self):
+        self._testLengthsImpl(0, 1)
+        self._testLengthsImpl(0, 3)
+        self._testLengthsImpl(400, 2)
+        self._testLengthsImpl(20, 1)
 
 if __name__ == '__main__':
     unittest.main()
