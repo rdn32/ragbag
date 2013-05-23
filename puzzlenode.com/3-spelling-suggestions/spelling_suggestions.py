c17c49adc6d8c113c90d818792a31d7bd8abf879
Robin Newton <rnewton@fastmail.fm>
2013-05-24 00:03:57 +0100

    In commonSubsequenceLength, ensure memoisation table fully populated before being needed

--- puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
+++ puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
@@ -11,6 +11,7 @@ def commonSubsequenceLength(s1, s2):
     # of s2.
     def lcs(l1, l2):
         if (l1,l2) not in memos:
+            assert populating
             if l1 == 0 or l2 == 0:
                 result = 0
             elif s1[l1-1] == s2[l2-1]:
@@ -20,4 +21,9 @@ def commonSubsequenceLength(s1, s2):
                              lcs(l1, l2-1))
             memos[l1,l2] = result
         return memos[l1,l2]
+    populating = True
+    for l1 in range(0, 1 + len(s1)):
+        for l2 in range(0, 1 + len(s2)):
+            lcs(l1, l2)
+    populating = False
     return lcs(len(s1), len(s2))
