7d79380e0c68e994d37e423d04d032f83f4eb4e3
Robin Newton <rnewton@fastmail.fm>
2013-05-23 23:58:41 +0100

    More tweaks to commonSubsequenceLength

--- puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
+++ puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 
 # Solution to "Spelling Suggestions" puzzle
+
+# Finds the length of the longest common subsequence
+# of two strings.
 def commonSubsequenceLength(s1, s2):
     memos = {}
-    def lcs(s1, s2):
-        if (s1,s2) not in memos:
-            if s1 == "" or s2 == "":
+    # Finds length of longest common subsequence of
+    # last l1 characters of s1 and last l2 characters
+    # of s2.
+    def lcs(l1, l2):
+        if (l1,l2) not in memos:
+            if l1 == 0 or l2 == 0:
                 result = 0
-            elif s1[0] == s2[0]:
-                result = 1 + lcs(s1[1:], s2[1:])
+            elif s1[l1-1] == s2[l2-1]:
+                result = 1 + lcs(l1-1, l2-1)
             else:
-                result = max(lcs(s1[1:], s2),
-                             lcs(s1, s2[1:]))
-            memos[s1,s2] = result
-        return memos[s1,s2]
-    return lcs(s1, s2)
+                result = max(lcs(l1-1, l2),
+                             lcs(l1, l2-1))
+            memos[l1,l2] = result
+        return memos[l1,l2]
+    return lcs(len(s1), len(s2))
