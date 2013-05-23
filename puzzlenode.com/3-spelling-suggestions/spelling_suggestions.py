5ec20e237b3ecd71028eb60d554587c57ce14289
Robin Newton <rnewton@fastmail.fm>
2013-05-23 23:49:00 +0100

    Use memoisation to improve performance of commonSubsequenceLength

--- puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
+++ puzzlenode.com/3-spelling-suggestions/spelling_suggestions.py
@@ -2,10 +2,16 @@
 
 # Solution to "Spelling Suggestions" puzzle
 def commonSubsequenceLength(s1, s2):
-    if s1 == "" or s2 == "":
-        return 0
-    elif s1[0] == s2[0]:
-        return 1 + commonSubsequenceLength(s1[1:], s2[1:])
-    else:
-        return max(commonSubsequenceLength(s1[1:], s2),
-                   commonSubsequenceLength(s1, s2[1:]))
+    memos = {}
+    def lcs(s1, s2):
+        if (s1,s2) not in memos:
+            if s1 == "" or s2 == "":
+                result = 0
+            elif s1[0] == s2[0]:
+                result = 1 + lcs(s1[1:], s2[1:])
+            else:
+                result = max(lcs(s1[1:], s2),
+                             lcs(s1, s2[1:]))
+            memos[s1,s2] = result
+        return memos[s1,s2]
+    return lcs(s1, s2)
